a1012e962a72dc11d916265248e279b196976b5a
Nofi Cahyono <noficahyono8@gmail.com>
2022-06-20 15:55:43 +0700

    feat: Tantangan Intro React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,22 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = ({data}) => {
     return (
-        <></>
+        <>
+          <div className='container'>
+            <div className='card'>
+              <img className='img' src={data.photo} alt={data.name} />
+              <div className='content'>
+                <h3>{data.name}</h3>
+                <p>{data.phone}</p>
+                <p>{data.email}</p>
+              </div>
+            </div>
+          </div>
+        </>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <></>
+        <>
+          <h1 className='header'>Call a Friend</h1>
+          <span>Your Friendly Contact App</span>
+          <p className='star'>**********************************************************************</p>
+        </>
     )
 }
 
